37681b2fd79ffa68ec9a4fa17b703b955da46915
Christine Kim <ckim328@uwo.ca>
2020-12-08 22:56:31 -0500

    Redicrecting

--- background.js
+++ background.js
@@ -1,10 +1,23 @@
 
 
+
+
+findURL = function changeURL(text){
+    console.log('this')
+    var current = window.location.href;
+    if(current === text){
+      window.location.replace("https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c");
+    }
+  }
+
+findURL("https://www.youtube.com/");
+var on = true;
+
 document.addEventListener('DOMContentLoaded', function () {
-    var contents = document.getElementById('content')
+    // var contents = document.getElementById('pleaseClose')
     var close = document.getElementById('pleaseClose')
     function remove(){
-            contents.remove()
+   
             console.log('sdf')
     }
 	close.addEventListener('click', function () {
