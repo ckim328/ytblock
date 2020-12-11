6faada6594d54bd7aec1428b30cb6c7af6259169
Christine Kim <ckim328@uwo.ca>
2020-12-10 22:47:49 -0500

    Added changing text

--- background.js
+++ background.js
@@ -1,27 +1,29 @@
 
-
+var toggle = true
 
 
 findURL = function changeURL(text){
     console.log('this')
     var current = window.location.href;
-    if(current === text){
+    if(current === text && toggle==true){
+
       window.location.replace("https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c");
     }
   }
 
 findURL("https://www.youtube.com/");
-var on = true;
+
 
 document.addEventListener('DOMContentLoaded', function () {
     // var contents = document.getElementById('pleaseClose')
     var close = document.getElementById('pleaseClose')
     function remove(){
-   
+            toggle = true
+            close.innerHTML="The toggle is on"
             console.log('sdf')
     }
 	close.addEventListener('click', function () {
 		remove();
 	});
-
+  findURL("https://www.youtube.com/");
 })
