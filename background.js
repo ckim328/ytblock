c6592b3f5178840228f490012577b0e2bbf2abb4
Christine Kim <ckim328@uwo.ca>
2020-12-15 12:35:52 -0500

    Added function toggle

--- background.js
+++ background.js
@@ -1,29 +1,35 @@
+var toggle = true;
 
-var toggle = true
+findURL = function changeURL(text) {
+	console.log('this');
+	var current = window.location.href;
+	if (current === text && toggle == true) {
+		window.location.replace(
+			'https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c'
+		);
+	}
+};
 
-
-findURL = function changeURL(text){
-    console.log('this')
-    var current = window.location.href;
-    if(current === text && toggle==true){
-
-      window.location.replace("https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c");
-    }
-  }
-
-findURL("https://www.youtube.com/");
-
+findURL('https://www.youtube.com/');
 
 document.addEventListener('DOMContentLoaded', function () {
-    // var contents = document.getElementById('pleaseClose')
-    var close = document.getElementById('pleaseClose')
-    function remove(){
-            toggle = true
-            close.innerHTML="The toggle is on"
-            console.log('sdf')
-    }
+	// var contents = document.getElementById('pleaseClose')
+	var close = document.getElementById('pleaseClose');
+
 	close.addEventListener('click', function () {
-		remove();
+
+		findURL = function changeURL(text) {  
+      toggle = !toggle;
+			console.log('this');
+			var current = window.location.href;
+			if (current === text && toggle == true) {
+				window.location.replace(
+					'https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c'
+				);
+			}
+		};
+
+		findURL('https://www.youtube.com/');
 	});
-  findURL("https://www.youtube.com/");
-})
+	findURL('https://www.youtube.com/');
+});
