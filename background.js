c543bb634aea6b251001157a44f76001126d1de6
Christine Kim <ckim328@uwo.ca>
2020-12-20 13:24:32 -0500

    Add butotn functinonality

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 var toggle = true;
 var lists = ['https://www.reddit.com/','https://www.youtube.com/']
 findURL = function changeURL() {
-	console.log('this');
 	var current = window.location.href;
 	if (lists.indexOf(current)>=0  && toggle == true) {
 		window.location.replace(
@@ -13,25 +12,19 @@ findURL = function changeURL() {
 findURL('https://www.youtube.com/');
 
 document.addEventListener('DOMContentLoaded', function () {
-	// var contents = document.getElementById('pleaseClose')
-	var close = document.getElementById('pleaseClose');
-	var lists = document.getElementById('lists');
-	close.addEventListener('click', function () {
 
-	findURL = function changeURL() {  
-      toggle = !toggle;
-			console.log('this');
-			var current = window.location.href;
-			if (lists.indexOf(current)>=0 && toggle == true) {
-				window.location.replace(
-					'https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c'
-				);
-			}
-		};
-		if (lists.indexOf(str) >= 0) {
-			//do something
+	var add = document.getElementById('add');
+	var listsShow = document.getElementById('lists');
+
+	add.addEventListener('click', function () {
+		console.log(listsShow.getElementsByTagName("li").length)
+		if (listsShow.getElementsByTagName("li").length !=lists.length){
+			lists.forEach(function (item) {
+				let li = document.createElement('li');
+				listsShow.appendChild(li);
+				listsShow.innerHTML += item;
+			});
 		}
-		findURL();
 	});
 	findURL();
 
