a4e6fdc1dcb90d196c7f58ca007ea7460d430a44
Christine Kim <ckim328@uwo.ca>
2020-12-28 15:14:13 -0500

    revereted

--- background.js
+++ background.js
@@ -1,54 +1,81 @@
 var toggle = true;
-var listsShow = ['https://www.reddit.com/','https://www.youtube.com/']
+var listsShow = ['https://www.youtube.com/'];
 
-findURL = function changeURL() {
-	var current = window.location.href;
-	if (listsShow.indexOf(current)>=0  && toggle == true) {
-		window.location.replace(
-			'https://www.notion.so/fb38a7d0c8454758a34aa99a694e557c'
-		);
+findURL = function changeURL(url, tabid) {
+	var block = document.getElementById('add');
+	if (listsShow.indexOf(url) >= 0 && block.innerText=="On") {
+		console.log('IN HERE', tabid);
+		chrome.tabs.remove(tabid);
 	}
 };
 
-
-async function showList(){
-	var listsShowEl = document.getElementById('lists');
-	if (listsShow.length ==0){
-		console.log('lists', listsShowEl)
-		listsShowEl.innerHTML =""
-	}
-	console.log(listsShow)
-	if (listsShowEl.getElementsByTagName("li").length !=listsShow.length && listsShow.length>0){
-		listsShow.forEach(function (item) {
-			let li = document.createElement('li');
-			li.appendChild(document.createTextNode(item))
-			listsShowEl.appendChild(li);
-			// listsShowEl.innerHTML += item;
-		});
-	}
+const getToggle = function getToggle(){
+	chrome.storage.local.get(['key'], function(result) {
+		console.log('Value currently is ' + result.key);
+	  });
+	
 }
+
+const setToggle = function setToggle(){
+	var currToggle = null
+	chrome.storage.local.get(['key'], function(result) {
+		currToggle = result.key
+		console.log('Value currently is ' + result.key);
+	  });
+	if (currToggle==undefined){
+		currToggle = true
+	}
+	chrome.storage.local.set({key: !currToggle}, function() {
+		console.log('Value is set to ' + !currToggle);
+	  });
+}
+const getActiveUrl = (tabid, changeInfo, tab) => {
+	const url = changeInfo.url;
+
+	// url is likely to be empty, and filter chrome:// and about:// URLs
+	if (!url || ['chrome://', 'about://'].some((p) => url.startsWith(p)))
+		return;
+
+	// filtering is not an active tab
+	if (!tab.active) return;
+
+	// the url address you need
+	console.log('getactive', changeInfo);
+	findURL(url, tab.id);
+};
+
+chrome.tabs.onUpdated.addListener(getActiveUrl);
+
+chrome.tabs.onActivated.addListener(function (activeInfo) {
+	chrome.tabs.get(activeInfo.tabId, function (tab) {
+		y = tab.url;
+		console.log('you are here: ' + tab);
+		findURL(y, activeInfo.tabId);
+	});
+});
+
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({ url: chrome.runtime.getURL("notes.html") });
+});
+
 
 document.addEventListener('DOMContentLoaded', function () {
 
-	var add = document.getElementById('add');
-	var clear = document.getElementById('clear');
-	var listsShowEl = document.getElementById('lists');
-	var txtBox = document.getElementById('txtbox');
-	add.addEventListener('click', function () {
-		console.log(listsShowEl.getElementsByTagName("li").length)
-		txt = txtBox.value;
-		console.log(txt)
-		if (txt.length>0){
-			listsShow.push(txt)
-			showList();
-		}
-	});
-	
-	clear.addEventListener('click', function(){
-		listsShow = [];
-		console.log(listsShow)
-		 showList();
-	})
-	findURL();
+	var block = document.getElementById('add');
+	var toggleVal = getToggle()
+	console.log('eve', window.location.href)
+	if (block ){
 
+		block.addEventListener('click', function () {
+			if (toggleVal){
+				block.innerText = "On"
+
+			}
+			else{
+				block.innerText = "Off"
+
+			}
+			setToggle()
+		});
+	}
 });
